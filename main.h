5ef6f6d79e030b99989db90cd10bdc0e3ed8c062
reem mohamed <ermohamed287@gmail.com>
2023-04-23 13:29:28 -0700

    9

--- main.h
+++ main.h
@@ -79,7 +79,7 @@ int print_pointer(va_list type, char buf[],
 	int flag, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
-int get_flags(const char *format, int *i);
+int get_flag(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
